49fca779686d9b7f531f81583352e1556ac5fb2b
Emmy <emerentia.arjani@gmail.com>
2021-03-24 22:34:19 +0700

    added form reset function and stopped submit button from refreshing page

--- library.js
+++ library.js
@@ -1,3 +1,4 @@
+//default library values
 let myLibrary = [
   new Book("Crime and Punishment", "Dostoeyvsky", 400, "yes"),
   new Book("Anna Karenina", "Leo Tolstoy", 800, "yes"),
@@ -17,8 +18,13 @@ Book.prototype.read = function () {
   this.hasread = (this.hasread === "yes")? "no" : "yes";
 }
 
+//Setting initial constants
 const bookshelf = document.getElementById("bookshelf");
 const submit = document.getElementById("submit");
+
+//if storage is local storage/firebase etc.
+let storage = "";
+
 
 //makes sure each newly generated book has event listener attached.
 bookshelf.addEventListener("click", function(e) {
@@ -28,7 +34,6 @@ bookshelf.addEventListener("click", function(e) {
     console.log("button of bookshelf clicked!");
     index = grabIndex(e.target.id);
     deleteBook(index);
-
   } 
   //read button
   else if (e.target && getButtonName(e.target.id) == "read" ) {
@@ -43,6 +48,26 @@ bookshelf.addEventListener("click", function(e) {
 drawBookshelf(myLibrary);
 console.log(myLibrary);
 
+//Event listener to add book to library
+submit.addEventListener("click", function () {
+  //there will be new values every time, hence need to be set in this func
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let hasread = (document.getElementById("has-read").checked)? "yes" : "no";
+
+  //check if form variables are blank
+  if (![title, author, pages].includes("")) {
+    let latestBook = new Book(title, author, pages, hasread);
+    resetForm();
+    addBook(latestBook);
+  }
+  console.log(myLibrary);
+  drawBookshelf(myLibrary);
+})
+
+//helper methods
+
 function grabIndex(string) {
   return string.split("-")[1];
 }
@@ -51,27 +76,16 @@ function getButtonName (string) {
   return string.split("-")[0];
 }
 
-//Event listener to add book to library
-submit.addEventListener("click", function () {
-  console.log("I am being clicked!");
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let hasread = (document.getElementById("has-read").checked)? "yes" : "no";
-
-  let variables = [title, author, pages];
-
-  if (!variables.includes("")) {
-    let latestBook = new Book(title, author, pages, hasread);
-    addBook(latestBook);
-    drawBookshelf(myLibrary);
-  }
-  console.log(myLibrary.length);
-
-})
-
-
 //action methods
+
+function resetForm() {
+  //reset form after every successfully created book
+  let input_defaults = {"title": "", "author": "", "pages": ""}
+  for (let key in input_defaults) {
+    document.getElementById(key).value = input_defaults[key];
+  }
+  document.getElementById("has-read").checked = false;
+}
 
 function deleteBook(index) {
   myLibrary.splice(index, 1);  
@@ -93,31 +107,24 @@ function clearBookshelf(bookshelf) {
 }
 
 function drawBookshelf(library) {
-  clearBookshelf(bookshelf);
-  console.log(library);
-  for (let i=0; i < library.length; i++) {
-    console.log(i);
-    //the indexes (id) of books will change with every book added and deleted, to correspond correctly with the index of the list (myLibrary)
-    let book = document.createElement("article");
-    book.insertAdjacentHTML("afterbegin", `
-    <p class='title'>${library[i].title}</p>
-    <p class='author'>By: ${library[i].author}</p>
-    <p class='pages'>No. of pages: ${library[i].pages}</p>
-    <p class='hasread'>Read?: ${library[i].hasread}</p>
-    <button id="delete-${i}" class="delete" type="button">Delete</button>
-    <button id="read-${i}" class="read" type="button">Read</button>
-    `);
-    bookshelf.appendChild(book);
+  if (library) {
+    clearBookshelf(bookshelf);
+    console.log(library);
+    for (let i=0; i < library.length; i++) {
+      //the indexes (id) of books will change with every book added and deleted, to correspond correctly with the index of the list (myLibrary)
+      let book = document.createElement("article");
+      book.insertAdjacentHTML("afterbegin", `
+      <p class='title'>${library[i].title}</p>
+      <p class='author'>By: ${library[i].author}</p>
+      <p class='pages'>No. of pages: ${library[i].pages}</p>
+      <p class='hasread'>Read?: ${library[i].hasread}</p>
+      <button id="delete-${i}" class="delete" type="button">Delete</button>
+      <button id="read-${i}" class="read" type="button">Read</button>
+      `);
+      bookshelf.appendChild(book);
+    }
   }
 }
 
 
 //Saving to Local Storage
-
-function saveToLocalStorage () {
-
-}
-
-function loadLocalStorage () {
-  //make sure if no array found, set default books/empty array
-}
