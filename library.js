e45c9ba2ccdfc80e93d8a0eda02d4ec5ab635cda
Emmy <emerentia.arjani@gmail.com>
2021-03-26 18:39:38 +0700

    added default library and cleaned up code

--- library.js
+++ library.js
@@ -1,11 +1,4 @@
-//default library values
-let myLibrary = [
-  new Book("Crime and Punishment", "Dostoeyvsky", 400, "yes"),
-  new Book("Anna Karenina", "Leo Tolstoy", 800, "yes"),
-  new Book("War and Peace", "Leo Tolstoy", 1440, "no"),
-  new Book("1984", "George Orwell", 300, "yes"),
-  new Book("Pastoralia", "George Saunders", 150, "yes")
-];
+let myLibrary = [];
 
 function Book(title, author, pages, hasread) {
     this.title = title;
@@ -22,18 +15,29 @@ Book.prototype.read = function () {
 const bookshelf = document.getElementById("bookshelf");
 const submit = document.getElementById("submit");
 
+const default_library = [
+  new Book("Crime and Punishment", "Fyodor Dostoevsky", 481, "yes"),
+  new Book("1984", "George Orwell", 328, "yes"),
+  new Book("War and Peace", "Leo Tolstoy", 1225, "no"),
+  new Book("Pastoralia", "George Saunders", 208, "yes"),
+  new Book("The Three Musketeers", "Alexandre Dumas", 539, "yes"),
+  new Book("CivilWarLand In Bad Decline", "George Saunders", 192, "yes"),
+  new Book("The Brothers Karamazov", "Fyodor Dostoevsky", 840, "no"),
+  new Book("A Streetcar Named Desire", "Tennessee Williams", 111, "no"),
+  new Book("Anna Karenina", "Leo Tolstoy", 864, "yes"),
+  new Book("The Great Gatsby", "F. Scott Fitzgerald", 288, "no")
+];
+
 //makes sure each newly generated book has event listener attached.
 bookshelf.addEventListener("click", function(e) {
 
   //delete button
   if (e.target && getButtonName(e.target.id) == "delete" ) {
-    console.log("button of bookshelf clicked!");
     index = grabIndex(e.target.id);
     deleteBook(index);
   } 
   //read button
   else if (e.target && getButtonName(e.target.id) == "read" ) {
-      console.log("button of bookshelf clicked! 2");
       index = grabIndex(e.target.id);
       readBook(index);
   }
@@ -49,20 +53,18 @@ submit.addEventListener("click", function () {
   let pages = document.getElementById("pages").value;
   let hasread = (document.getElementById("has-read").checked)? "yes" : "no";
 
-  //checks if form variables are blank
+  //checks if form variables are blank (if not, create new book)
   if (![title, author, pages].includes("")) {
     let latestBook = new Book(title, author, pages, hasread);
     addBook(latestBook);
     resetForm();
     closeForm();
+    saveLocalStorage();
   }
-  console.log(myLibrary);
-  //maybe only save if book successfully created?
-  saveLocalStorage();
   drawBookshelf(myLibrary);
 })
 
-//helper methods
+//Methods (Book/Bookshelf)
 
 function grabIndex(string) {
   return string.split("-")[1];
@@ -70,16 +72,6 @@ function grabIndex(string) {
 
 function getButtonName (string) {
   return string.split("-")[0];
-}
-
-//action methods
-
-function resetForm() {
-  let input_defaults = ["title", "author", "pages"];
-  for (let i=0; i < input_defaults.length; i++) {
-    document.getElementById(input_defaults[i]).value = "";
-  }
-  document.getElementById("has-read").checked = false;
 }
 
 function deleteBook(index) {
@@ -101,7 +93,6 @@ function clearBookshelf(bookshelf) {
 function drawBookshelf(library = myLibrary) {
   if (library) {
     clearBookshelf(bookshelf);
-    // console.log(library);
     for (let i=0; i < library.length; i++) {
       //the indexes (id) of books will change with every book added and deleted, to correspond correctly with the index of the list (myLibrary)
       let book = document.createElement("article");
@@ -126,17 +117,13 @@ function drawBookshelf(library = myLibrary) {
 
 document.getElementById("reset-db").addEventListener("click", function() {
   localStorage.clear();
-  // console.log(myLibrary);
-  // console.log(localStorage);
   myLibrary = [];
-  //draw function for some reason not working
   drawBookshelf(myLibrary);
 })
 
 
 function saveLocalStorage() {
   localStorage.setItem("library", JSON.stringify(myLibrary));
-  // console.log(localStorage);
 }
 
 function loadLocalStorage() {
@@ -147,21 +134,29 @@ function loadLocalStorage() {
     for (let i=0; i < db.length; i++) {
       library.push(new Book(db[i]["title"], db[i]["author"], db[i]["pages"], db[i]["hasread"]));
     }
+  } else {
+    library = default_library;
   }
   return library;
 }
 
-//funcion for popup open and close form
+//Form
 
 document.getElementById("open-form").addEventListener("click", function(){
   openForm();
 })
 
-//if click mask, close form (and mask)
-
 document.getElementById("mask").addEventListener("click", function() {
   closeForm();
 })
+
+function resetForm() {
+  let input_defaults = ["title", "author", "pages"];
+  for (let i=0; i < input_defaults.length; i++) {
+    document.getElementById(input_defaults[i]).value = "";
+  }
+  document.getElementById("has-read").checked = false;
+}
 
 
 function openForm() {
@@ -175,7 +170,7 @@ function closeForm() {
 }
 
 
-//set and draw library when webpage first loaded
+//setup and draw library when webpage is first loaded
 
 myLibrary = loadLocalStorage();
 drawBookshelf(myLibrary);
